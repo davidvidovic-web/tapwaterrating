4a401fd33f1d4846cab1590729e7fd070bdcbe78
davidvidovic-web <mail@davidvidovic.com>
2026-01-03 23:55:11 +0100

    changed maps to use cartodb voyager

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -310,8 +310,8 @@ export const Map = forwardRef<MapHandle, Props>(({ cities, reviews = [], onSelec
     >
       <MapRefHandler onMapReady={handleMapReady} />
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         maxZoom={19}
         minZoom={2}
         keepBuffer={2}
